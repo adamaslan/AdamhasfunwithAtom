8526ab70ed0c208476ebd29c9e75674b38605e26
Adam Aslan <chillcoders@gmail.com>
2021-06-23 15:10:08 -0400

    redux1

---
+++ src/redux/reducers/index.js
@@ -0,0 +1,12 @@
+import { combineReducers } from "redux";
+//import { useSelector, TypedUseSelectorHook } from "react-redux";
+import themeReducer from "./themeReducer";
+import ideasReducer from "./ideasReducer";
+
+const reducers = combineReducers({
+  ideas: ideasReducer,
+  theme: themeReducer,
+  user: () => ({ name: "Nick" }),
+});
+
+export default reducers;
