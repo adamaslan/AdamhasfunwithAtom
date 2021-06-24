36aeca0b66b814765ff2cd097a9bdab21d584f75
Adam Aslan <chillcoders@gmail.com>
2021-06-24 12:53:59 -0400

    big-update

--- src/HappyBoi.js
+++ src/HappyBoi.js
@@ -1,1 +1,28 @@
 //HappyBoi.js
+
+//Ideas.js
+
+import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+
+
+
+export default function Ideas({ stuff }) {
+  const ideasData = useSelector((reduxState) => reduxState.ideas);
+
+  const dispatch = useDispatch();
+
+  console.log(ideasData);
+  const handleSelect = () => {
+    dispatch({
+      type: "SelectCategory",
+      payload: "fun",
+    });
+  };
+  return (
+    <div>
+      <p>Ideas</p>
+      <button onClick={handleSelect}> "SelectCategory" </button>
+    </div>
+  );
+}
